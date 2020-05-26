4bc5e3818221dc13f48c32874a11561617cb8ca8
wrongway213 <tropeazy@gmail.com>
2020-05-26 08:19:52 -0400

    Coral: Remove GCS It's in GApps now

--- coral/coral-vendor.mk
+++ coral/coral-vendor.mk
@@ -50,7 +50,6 @@ PRODUCT_PACKAGES += \
     DreamlinerUpdater \
     EuiccGoogle \
     EuiccSupportPixel \
-    GCS \
     grilservice \
     HardwareInfo \
     HotwordEnrollmentOKGoogleHEMIDELTA \
